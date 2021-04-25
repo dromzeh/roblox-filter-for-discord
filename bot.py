14c02717106414a5ba6cd08bd56af522390e6845
dromzeh <65048232+dromzeh@users.noreply.github.com>
2021-04-25 23:03:52 +0100

    Add files via upload
    uploading main files

---
+++ bot.py
@@ -0,0 +1,52 @@
+# Copyright © 2021 dromzeh
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import discord
+from discord.ext import commands
+from discord.ext.commands import has_permissions
+from discord.ext.commands import guild_only
+from aiohttp import ClientSession
+
+with open('bannedwords.txt', 'r') as x:
+    global bannedwords
+
+    words = x.read()
+    bannedwords = words.split()
+
+prefix = 'rr!' # u dont need to change this. the bot has no commands, in reality, you can put whatever you want here.
+
+TOKEN = "" # put your bot token in here
+WEBHOOK_URL = "" # enter the webhook url to put the tagged messages into
+CHANNEL_ID = 123456789 # change this to the channel id to 'moderate' (also must go to the the same channel as the webhook channel)
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix=prefix, intents=intents)
+bot.remove_command('help')
+
+@bot.event
+async def on_ready():
+
+    print(f'logged in as {bot.user.name} [{bot.user.id}]')
+    print(f'bot coded by https://github.com/dromzeh, licensed under MIT')
+
+@bot.event
+async def on_message(message):
+
+    msg = message.content.lower()
+    channel = message.channel
+
+    if channel.id == CHANNEL_ID:
+
+        for word in bannedwords:
+            if word in msg:
+                await message.delete()
+
+                messagelength = len(msg)
+
+                async with ClientSession() as session:
+                    webhook = discord.Webhook.from_url(WEBHOOK_URL, adapter=discord.AsyncWebhookAdapter(session))
+                    await webhook.send(content="#" * messagelength, username=message.author.name, avatar_url=message.author.avatar_url) 
+
+bot.run(TOKEN)
